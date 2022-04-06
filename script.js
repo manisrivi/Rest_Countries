3a81ec16a546fe8a122b25d785bb41cff81670ae
MANIKANDAN S <95971849+manisrivi@users.noreply.github.com>
2022-04-06 12:32:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let res = fetch("https://restcountries.com/v3.1/all");
+res.then((data) => data.json())
+    .then(function (countrydata) {
+
+        // Element tag create function
+        function foo(ele, cname) {
+            let div = document.createElement(ele);
+            div.className = cname;
+            return div;
+        }
+        // container 
+        let container = foo("div", "container");
+        document.body.append(container);
+
+        // row
+        let row = foo("div", "row m-3");
+        container.append(row);
+
+        // Cards display 
+        countrydata.forEach(function (country) {
+            let col = foo("div", "col-lg-3 col-sm-12 col-md-6")
+            col.innerHTML = `<div class="card m-3 zoom shadow-lg  p-1 bg-body rounded" style="width: 18rem;">
+       <img height="200" src="${country.flags.png}" class="card-img-top" alt="...">
+       <div class="card-body text-center">
+         <h6 class="card-title fw-bold">Country: ${country.name.common}<br><br>
+           Capital: ${country.capital} <br><br>
+           Region: ${country.region} <br>
+         </h6>
+       </div>
+     </div>`
+            row.append(col)
+        })
+    })
+    .catch((error) => console.log(error));
+
